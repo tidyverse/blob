1ff83a95c61014d907c9777cafac2abde1f3378a
Kirill Müller <krlmlr+r@mailbox.org>
2019-01-15 17:12:40 +0100

    is.na() works out of the box, is.na<-() needs to be kept for now

--- R/missing.R
+++ R/missing.R
@@ -1,9 +1,4 @@
 #' @export
-is.na.blob <- function(x) {
-  vapply(x, is.null, logical(1))
-}
-
-#' @export
 `is.na<-.blob` <- function(x, value) {
   if (!is.logical(value) || length(x) != length(value)) {
     stop("RHS must be a logical the same length as `x`", call. = FALSE)
