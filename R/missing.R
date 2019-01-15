6700523c5ec9d81ca5d591537b03fefd04a8cc4c
Kirill Müller <krlmlr+r@mailbox.org>
2019-01-15 17:13:44 +0100

    - Simplify .

--- R/missing.R
+++ R/missing.R
@@ -1,9 +1,10 @@
+# No implementation in vctrs 0.1.0
 #' @export
 `is.na<-.blob` <- function(x, value) {
   if (!is.logical(value) || length(x) != length(value)) {
     stop("RHS must be a logical the same length as `x`", call. = FALSE)
   }
 
-  x[value] <- rep(list(NULL), sum(value))
+  x[value] <- list(NULL)
   x
 }
