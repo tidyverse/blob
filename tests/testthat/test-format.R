5519ce90f8b7e28a244630f1ad5f0982b3f60391
Kirill Müller <krlmlr+r@mailbox.org>
2019-02-09 16:44:29 +0100

    Add known output test

--- tests/testthat/test-format.R
+++ tests/testthat/test-format.R
@@ -45,3 +45,11 @@ test_that("similar sizes", {
     )
   )
 })
+
+test_that("output", {
+  skip_on_cran()
+
+  x <- blob(raw(2 ** 2), raw(2 ** 11), raw(2 ** 20))
+
+  expect_known_output(print(x), "blob.txt")
+})
