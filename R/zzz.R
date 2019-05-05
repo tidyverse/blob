4a624c11edebc6b2e89efceed80b8fda6f681743
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-05 20:43:25 +0200

    Need utils

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   s3_register("pillar::pillar_shaft", "blob")
   s3_register("pillar::is_vector_s3", "blob")
 
-  if (packageVersion("vctrs") <= "0.1.0") {
+  if (utils::packageVersion("vctrs") <= "0.1.0") {
     vec_default_cast <<- vec_default_cast_old
   } else {
     rm("vec_default_cast", inherits = TRUE)
