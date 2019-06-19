141a62de7cbcc27b79381db76adcdf7d01c0b359
Kirill Müller <krlmlr+r@mailbox.org>
2019-06-19 23:09:07 +0200

    Adapt to dev vctrs

--- R/cast.R
+++ R/cast.R
@@ -8,49 +8,37 @@
 #' @method vec_cast blob
 #' @export
 #' @export vec_cast.blob
-vec_cast.blob <- function(x, to) UseMethod("vec_cast.blob")
+vec_cast.blob <- function(x, to, ...) UseMethod("vec_cast.blob")
 
 #' @method vec_cast.blob default
 #' @export
-vec_cast.blob.default <- function(x, to) vec_default_cast(x, to)
+vec_cast.blob.default <- function(x, to, ...) vec_default_cast(x, to, ...)
 
 #' @method vec_cast.blob blob
 #' @export
-vec_cast.blob.blob <- function(x, to) x
+vec_cast.blob.blob <- function(x, to, ...) x
 
 #' @method vec_cast.blob list
 #' @export
-vec_cast.blob.list <- function(x, to) blob(!!!x)
+vec_cast.blob.list <- function(x, to, ...) blob(!!!x)
 
 #' @method vec_cast.blob vctrs_list_of
 #' @export
-vec_cast.blob.vctrs_list_of <- function(x, to) {
+vec_cast.blob.vctrs_list_of <- function(x, to, ...) {
   new_blob(vec_cast(x, new_list_of(ptype = raw())))
 }
 
 #' @method vec_cast.blob integer
 #' @export
-vec_cast.blob.integer <- function(x, to) {
+vec_cast.blob.integer <- function(x, to, ...) {
   signal_soft_deprecated("Coercing an integer vector to a blob is deprecated, please coerce to a list first.")
   blob(!!!lapply(x, as_single_raw))
 }
 
 #' @method vec_cast.blob raw
 #' @export
-vec_cast.blob.raw <- function(x, to) blob(x)
+vec_cast.blob.raw <- function(x, to, ...) blob(x)
 
 #' @method vec_cast.blob character
 #' @export
-vec_cast.blob.character <- function(x, to) blob(!!!lapply(x, charToRaw))
-
-# Requires vctrs > 0.1.0
-vec_default_cast <- NULL
-
-utils::globalVariables("vec_unspecified_cast")
-vec_default_cast_old <- function(x, to) {
-  if (is.logical(x)) {
-    vec_unspecified_cast(x, to)
-  } else {
-    stop_incompatible_cast(x, to)
-  }
-}
+vec_cast.blob.character <- function(x, to, ...) blob(!!!lapply(x, charToRaw))
