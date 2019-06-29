4b24e15469ec4237b259d0b32ee832b466e96139
Kirill Müller <krlmlr+r@mailbox.org>
2019-06-29 23:43:06 +0200

    Adapt to dev vctrs

--- R/coerce.R
+++ R/coerce.R
@@ -1,38 +1,38 @@
 #' Coercion
 #'
-#' Double dispatch methods to support [vctrs::vec_type2()].
+#' Double dispatch methods to support [vctrs::vec_ptype2()].
 #'
-#' @inheritParams vctrs::vec_type2
+#' @inheritParams vctrs::vec_ptype2
 #'
-#' @method vec_type2 blob
+#' @method vec_ptype2 blob
 #' @export
-#' @export vec_type2.blob
-vec_type2.blob <- function(x, y, ..., x_arg = "", y_arg = "") UseMethod("vec_type2.blob", y)
+#' @export vec_ptype2.blob
+vec_ptype2.blob <- function(x, y, ..., x_arg = "", y_arg = "") UseMethod("vec_ptype2.blob", y)
 
-#' @method vec_type2.blob default
+#' @method vec_ptype2.blob default
 #' @export
-vec_type2.blob.default <- function(x, y, ..., x_arg = "", y_arg = "") stop_incompatible_type(x, y, x_arg, y_arg)
+vec_ptype2.blob.default <- function(x, y, ..., x_arg = "", y_arg = "") stop_incompatible_type(x, y, x_arg, y_arg)
 
-#' @method vec_type2.blob blob
+#' @method vec_ptype2.blob blob
 #' @export
-vec_type2.blob.blob <- function(x, y, ...) {
+vec_ptype2.blob.blob <- function(x, y, ...) {
   new_blob(list())
 }
 
-#' @method vec_type2.blob vctrs_unspecified
+#' @method vec_ptype2.blob vctrs_unspecified
 #' @export
-vec_type2.blob.vctrs_unspecified <- function(x, y, ...) x
+vec_ptype2.blob.vctrs_unspecified <- function(x, y, ...) x
 
-#' @method vec_type2.blob list
+#' @method vec_ptype2.blob list
 #' @export
-vec_type2.blob.list <- function(x, y, ..., x_arg = "", y_arg = "") {
+vec_ptype2.blob.list <- function(x, y, ..., x_arg = "", y_arg = "") {
   check_raw_list(y, y_arg)
   new_blob(list())
 }
 
-#' @method vec_type2.list blob
+#' @method vec_ptype2.list blob
 #' @export
-vec_type2.list.blob <- function(x, y, ..., x_arg = "", y_arg = "") {
+vec_ptype2.list.blob <- function(x, y, ..., x_arg = "", y_arg = "") {
   check_raw_list(x, x_arg)
   new_blob(list())
 }
