378b24d08189f6eb1fc00bc6faeb507128cab89a
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-06-19 14:38:48 +0200

    Format package w/ air

--- R/util.R
+++ R/util.R
@@ -1,12 +1,14 @@
 is_raw_list <- function(x) {
-  if (!is.list(x))
+  if (!is.list(x)) {
     return(FALSE)
+  }
 
   raw <- vapply(x, is.raw, logical(1))
   null <- vapply(x, is.null, logical(1))
 
-  if (!all(raw | null))
+  if (!all(raw | null)) {
     return(FALSE)
+  }
 
   TRUE
 }
